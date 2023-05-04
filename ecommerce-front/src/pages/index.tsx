306d69ae99bb4bf02db9b3e8c70280c89ae425a0
hieugiabao <hieugiabao12@gmail.com>
2023-05-04 21:22:24 +0700

    feat: connect to mongodb

--- ecommerce-front/src/pages/index.tsx
+++ ecommerce-front/src/pages/index.tsx
@@ -1,7 +1,45 @@
-export default function Home() {
+import { mongooseConnect } from "@ft/lib/mongoose";
+import { NextPage } from "next";
+
+interface HomePageProps {
+  connected: boolean;
+  error?: string;
+}
+
+const HomePage: NextPage<HomePageProps> = ({ connected, error }) => {
+  if (!connected) {
+    return (
+      <>
+        <h1>There was an error connecting to the database</h1>
+        <p>{error}</p>
+      </>
+    );
+  }
   return (
     <>
-      <h1>Welcome to E-commerce app</h1>
+      <h1>Connected to the database</h1>
     </>
   );
+};
+
+export async function getServerSideProps() {
+  try {
+    await mongooseConnect();
+
+    return {
+      props: {
+        connected: true,
+      },
+    };
+  } catch (e: any) {
+    console.error(e);
+    return {
+      props: {
+        connected: false,
+        error: e.message,
+      },
+    };
+  }
 }
+
+export default HomePage;
